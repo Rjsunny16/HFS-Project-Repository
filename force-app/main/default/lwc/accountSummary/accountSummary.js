74381fbab22b54cfb48f27a23b8c523e64ec323a
Rochak Jain <sitesh13010798@gmail.com>
2021-09-11 15:57:11 +0530

    ch

--- force-app/main/default/lwc/accountSummary/accountSummary.js
+++ force-app/main/default/lwc/accountSummary/accountSummary.js
@@ -6,33 +6,54 @@ import fetchCase from '@salesforce/apex/AccountRelatedObj.fetchCase';
 import getClosedWonOpp from '@salesforce/apex/AccountRelatedObj.getClosedWonOpp';
 import getClosedLostOpp from '@salesforce/apex/AccountRelatedObj.getClosedLostOpp';
 import getAmountClosedWonOpp from '@salesforce/apex/AccountRelatedObj.getAmountClosedWonOpp';
+import countContacts from '@salesforce/apex/AccountRelatedObj.countContacts';
+import countCases from '@salesforce/apex/AccountRelatedObj.countCases';
+import countOpp from '@salesforce/apex/AccountRelatedObj.countOpp';
+import { updateRecord } from 'lightning/uiRecordApi';
+import ID_FIELD from '@salesforce/schema/Account.Id';
+import TARGET_REVENUE from '@salesforce/schema/Account.Full_Year_Target_Revenue__c';
+import Financial_Year from '@salesforce/schema/Account.Financial_Year__c';
+import Campaign_Budget from '@salesforce/schema/Account.Campaign_Budget__c';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 
-
-// import { getRecord, getFieldValue } from "lightning/uiRecordApi";
-// import Total_Opportunity from "@salesforce/schema/Account.Number_of_Opportunity__c";
-
-
-// const fields = [Total_Opportunity];
 export default class AccountSummary extends LightningElement {
-    // @api
-    // recordId;
-
-    // // Wire the output of the out of the box method getRecord to the property account
-    // @wire(getRecord, {
-    //             recordId: "$recordId",
-    //             fields
-    //         })
-    // account;
+    
     @track acc;
     @track con;
-    message;
-    msg;
+    @track message;
+    @track msg;
+    @track cntopp
+    @track cntcs;
+    @track cnt;
     @track opp;
     @track cs;
     @track closedwonofopp;
     @track closedlostopp;
     @track amountClosedWonOpp;
+    updateAccount(){
+      const fields = {};
+      fields[ID_FIELD.fieldApiName] = this.msg;
+      fields[Financial_Year.fieldApiName] = this.template.querySelector("[data-field='Financial_Year__c']").value;
+      fields[TARGET_REVENUE.fieldApiName] = this.template.querySelector("[data-field='Full_Year_Target_Revenue__c']").value;
+      fields[Campaign_Budget.fieldApiName] = this.template.querySelector("[data-field='Campaign_Budget__c']").value;
+      console.log(fields);
+
+      const recordInput = { fields };
+      updateRecord(recordInput)
+      .then(() => {
+          this.dispatchEvent(
+              new ShowToastEvent({
+                  title: 'Success',
+                  message: 'Account Updated',
+                  variant: 'success'
+              })
+          );
+      })
+      .catch(error => {
+          console.log(error);
+      });
+    }
     connectedCallback(){
                         fetchAccount()
                         .then(result => {
@@ -44,9 +65,7 @@ export default class AccountSummary extends LightningElement {
 
                         }
 
-    // get total() {
-    //         return getFieldValue(this.account.data, Total_Opportunity);
-    // }
+   
     contactFetch(event){
                             this.message = event.target.value;
                             console.log('Contact Id-->'+this.message);
@@ -62,7 +81,20 @@ export default class AccountSummary extends LightningElement {
                             this.error = error;
 
                                             })
+                            this.message = event.target.value;
+                            console.log('Contact Id-->'+this.message);
+                            countContacts({accountId : this.message})
 
+                            .then(result => {
+                                                this.cnt = result;
+
+                                                console.log(JSON.stringify(result));
+                                                console.log("result1",this.cnt);
+                                            })
+                            .catch(error =>{
+                            this.error = error;
+
+                                            })
                             this.msg = event.target.value;
                             console.log('Oppoertunity Id-->'+this.msg);
                             fetchOpportunity({accountId : this.msg})
@@ -77,6 +109,19 @@ export default class AccountSummary extends LightningElement {
 
                                             })
                             this.msg = event.target.value;
+                            console.log('Oppoertunity Id-->'+this.msg);
+                            countOpp({accountId : this.msg})
+                            .then(result => {
+                                              this.cntopp = result;
+
+                                              console.log(JSON.stringify(result));
+                                              console.log("result2",this.cntopp);
+                                            })
+                            .catch(error =>{
+                                            this.error = error;
+
+                                            })
+                            this.msg = event.target.value;
                             console.log('Case Id-->'+this.msg);
                             fetchCase({accountId : this.msg})
                             .then(result => {
@@ -84,6 +129,19 @@ export default class AccountSummary extends LightningElement {
 
                                               console.log(JSON.stringify(result));
                                               console.log("result2",this.cs);
+                                            })
+                            .catch(error =>{
+                                            this.error = error;
+
+                                            })
+                            this.msg = event.target.value;
+                            console.log('Case Id-->'+this.msg);
+                            countCases({accountId : this.msg})
+                            .then(result => {
+                                              this.cntcs = result;
+
+                                              console.log(JSON.stringify(result));
+                                              console.log("result",this.cntcs);
                                             })
                             .catch(error =>{
                                             this.error = error;
